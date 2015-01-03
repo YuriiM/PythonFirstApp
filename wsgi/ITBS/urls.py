9814afc98acddbea447e145d1bbda1a6bf81aa2c
Yurii <yuriimys@gmail.com>
2015-01-03 19:38:23 +0200

    Added functions in views.py and routes in urls.py

--- wsgi/ITBS/urls.py
+++ wsgi/ITBS/urls.py
@@ -4,9 +4,9 @@ from ITBS import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    #url(r'^select_clients/', views.select_clients, name='select_clients'),
-    #url(r'^delete_client/', views.delete_client, name='delete_client'),
-    #url(r'^add_client/', views.add_client, name='add_client'),
-    #url(r'^upd_prepare/', views.upd_prepare, name='upd_prepare'),
-    #url(r'^update_client/', views.update_client, name='update_client'),
+    url(r'^select_clients/', views.select_clients, name='select_clients'),
+    url(r'^delete_client/', views.delete_client, name='delete_client'),
+    url(r'^add_client/', views.add_client, name='add_client'),
+    url(r'^upd_prepare/', views.upd_prepare, name='upd_prepare'),
+    url(r'^update_client/', views.update_client, name='update_client'),
 )
--- wsgi/ITBS/views.py
+++ wsgi/ITBS/views.py
@@ -7,7 +7,86 @@ from django.shortcuts import get_object_or_404
 import json
 
 
-# Create your views here.
+# Index view.
 def index(request):
     form = ClientForm()
     return render(request, 'ITBS/index.html', {'form': form})
+
+
+# Get list of clients.
+def select_clients(request):
+    if request.is_ajax() and request.method == 'POST':
+        clients_list = Client.objects.all()
+        template = loader.get_template('ITBS/clTable.html')
+        context = RequestContext(request, {
+            'clients_list': clients_list,
+        })
+        return HttpResponse(template.render(context))
+    else:
+        return False
+
+
+# Delete client
+def delete_client(request):
+    if request.is_ajax() and request.method == 'POST':
+        query = json.loads((request.body).decode())
+        client_id = query['client_id']
+        Client.objects.filter(id=client_id).delete()
+        return HttpResponse(select_clients(request))
+    else:
+        return False
+
+
+# Add client
+def add_client(request):
+    if request.is_ajax() and request.method == 'POST':
+        query = json.loads((request.body).decode())
+        form = ClientForm(query)
+        if form.is_valid():
+            form.save()
+            return HttpResponse(select_clients(request))
+        else:
+            template = loader.get_template('ITBS/validation.html')
+            context = RequestContext(request, {
+                'form': form,
+            })
+            return HttpResponse(template.render(context))
+    else:
+        return False
+
+
+# Prepare form for update
+def upd_prepare(request):
+    if request.is_ajax() and request.method == 'POST':
+        query = json.loads((request.body).decode())
+        client_id = query['client_id']
+        chosen_client = Client.objects.get(id=client_id)
+        form = ClientForm(instance=chosen_client)
+        template = loader.get_template('ITBS/upForm.html')
+        context = RequestContext(request, {
+            'form': form,
+            'client_id': client_id,
+        })
+        return HttpResponse(template.render(context))
+    else:
+        return False
+
+
+# Update client
+def update_client(request):
+    if request.is_ajax() and request.method == 'POST':
+        query = json.loads((request.body).decode())
+        client_id=query['client_id']
+        instance = get_object_or_404(Client, id=client_id)
+        form = ClientForm(query, instance=instance)
+        if form.is_valid():
+            form.save()
+            return HttpResponse(select_clients(request))
+        else:
+            template = loader.get_template('ITBS/validation.html')
+            context = RequestContext(request, {
+                'form': form,
+            })
+            return HttpResponse(template.render(context))
+    else:
+        return False
